ce493701e1d0eeb19562262ce2b6b4ff0546c7bf
Marissa <marissa.shieh@gmail.com>
2019-05-06 20:29:11 -0700

    Create a cookie which expires

--- react-client/src/components/SaveBtn.jsx
+++ react-client/src/components/SaveBtn.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import moment from 'moment';
 
 function SaveBtn(props) {
 
   function saveToDatabase() {
     console.log('clicked',props);
-    console.log(document.cookie);
+    console.log('cookie', document.cookie);
+    if (!document.cookie) {
+      console.log('making new cookie');
+      var dateNow = new Date();
+      var newDateObj = moment.utc(dateNow).add(1, 'm');
+      var generatedUsername = `${Math.ceil(Math.random() * 100000) + Math.floor(Math.random() * 5000)}`
+      document.cookie = `username=${generatedUsername};expires=${newDateObj}`
+    } else {
+      console.log('cookie exists');
+    }
   }
 
   return (
